6ffff81eb3028b613ff57fc6f5b133f55dd6d08b
Erik <erik.linsdell@gmail.com>
2026-02-05 14:55:03 +1100

    Make page transitions smoother and longer - increase duration to 1000ms with better easing

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -16,16 +16,23 @@ const PageTransition = ({ children }: PageTransitionProps) => {
       const timer = setTimeout(() => {
         setDisplayLocation(location);
         setTransitionStage('entered');
-      }, 300);
+      }, 600); // Half of total transition time for fade out
       return () => clearTimeout(timer);
     }
   }, [location, displayLocation]);
 
   return (
     <div
-      className={`min-h-screen transition-opacity duration-300 ease-in-out ${
-        transitionStage === 'entering' ? 'opacity-0' : 'opacity-100'
+      className={`min-h-screen transition-all duration-1000 ease-[cubic-bezier(0.4,0,0.2,1)] ${
+        transitionStage === 'entering' 
+          ? 'opacity-0 translate-y-4' 
+          : 'opacity-100 translate-y-0'
       }`}
+      style={{
+        transitionProperty: 'opacity, transform',
+        transitionDuration: '1000ms',
+        transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
+      }}
     >
       {children}
     </div>
